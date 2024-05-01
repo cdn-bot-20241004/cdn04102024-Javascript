43c559ce5fc4e906bf2435a6800a88ac68a92d59
cdn <cdn@sixnews.dev>
2024-05-01 17:42:33 +0200

    Delete javascript/javascript directory

--- javascript/javascript/backHistory.js
+++
@@ -1,8 +0,0 @@
-const backLinks = document.querySelectorAll('.back-link');
-
-backLinks.forEach(backLink => {
-    backLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        history.back();
-    })
-});
