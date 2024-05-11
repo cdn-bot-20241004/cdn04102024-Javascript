9d8856249b85faf840562dbc5bffd899c05e9a98
cdn <cdn@sixnews.dev>
2024-05-11 20:58:21 +0200

    Update backtotop.js

--- javascript/backtotop.js
+++ javascript/backtotop.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    if (window.matchMedia("(min-width: 528px), (max-width: 319px)").matches) {
-        let button = document.querySelector(".buttonTop");
 
-        window.addEventListener('scroll', () => {
-            if (button) {
-                if (
-                    document.documentElement.scrollTop > 250
-                ) {
-                    button.style.display = "flex";
-                } else {
-                    button.style.display = "none";
-                }
+    let button = document.querySelector(".buttonTop");
+
+    window.addEventListener('scroll', () => {
+        if (button) {
+            if (
+                document.documentElement.scrollTop > 250
+            ) {
+                button.style.display = "flex";
+            } else {
+                button.style.display = "none";
             }
-        });
+        }
+    });
 
-        button.addEventListener("click", function () {
-            window.scrollTo({
-                top: 0,
-                behavior: "instant",
-            });
+    button.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "instant",
         });
-    }
+    });
+
 });
