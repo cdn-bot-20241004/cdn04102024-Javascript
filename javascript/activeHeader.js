ab6ea7e99d447c1b2fc52114e9fe7574ae44bd80
cdn <cdn@sixnews.dev>
2024-05-11 21:50:52 +0200

    Update activeHeader.js

--- javascript/activeHeader.js
+++ javascript/activeHeader.js
@@ -1,7 +1,9 @@
-var url = window.location.pathname;
-var links = document.querySelectorAll(".topnav a");
-links.forEach(function (link) {
-    if (link.getAttribute("href") === url) {
-        link.classList.add("active");
-    }
+window.addEventListener('DOMContentLoaded', function () {
+    var url = window.location.pathname;
+    var links = document.querySelectorAll(".topnav a");
+    links.forEach(function (link) {
+        if (link.getAttribute("href") === url) {
+            link.classList.add("active");
+        }
+    });
 });
